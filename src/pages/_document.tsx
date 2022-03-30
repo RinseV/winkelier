d839a8c68c59880618578c681bcd4e6eef74c1ec
RinseV <43185712+RinseV@users.noreply.github.com>
2022-03-30 14:33:02 +0200

    feat: favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,6 +10,7 @@ export default class _Document extends Document {
                     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
                     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
                     <link rel="manifest" href="/site.webmanifest" />
+                    <meta name="theme-color" content="#8CD867" />
                 </Head>
                 <body>
                     <ColorModeScript />
